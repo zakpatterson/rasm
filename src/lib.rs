3e02634b4bf90d74736b7e3e739a2dc76d888b1a
Zak Patterson <pattersonzak@gmail.com>
2018-11-10 16:51:30 -0600

    Figure out how to set inner html on a div!

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,22 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
+
+    type HTMLDocument;
+    type Element;
+
+    static document : HTMLDocument;
+
+    #[wasm_bindgen(method)]
+    fn getElementById(this: &HTMLDocument, tagName:&str) -> Element;
+
+    #[wasm_bindgen(method, setter=innerHTML)]
+    fn set_inner(this: &Element, html: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+
+    document.getElementById("app").set_inner("<p>HI THIS IS GOOD</p>");
 }
